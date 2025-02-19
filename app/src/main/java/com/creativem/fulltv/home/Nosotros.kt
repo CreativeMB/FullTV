cef81985ffd83f62a75a74b80e6d3bf167e635ba
TobiasM <ingcpu@gmail.com>
2025-02-18 21:09:47 -0500

    quinto

--- app/src/main/java/com/creativem/fulltv/home/Nosotros.kt
+++ app/src/main/java/com/creativem/fulltv/home/Nosotros.kt
@@ -3,7 +3,9 @@ package com.creativem.fulltv.home
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import com.creativem.fulltv.R
 
 class Nosotros : AppCompatActivity() {
@@ -13,7 +15,12 @@ class Nosotros : AppCompatActivity() {
         setContentView(R.layout.nosotros) // Asegúrate de tener este layout creado
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        val col1: TextView = findViewById(R.id.col1)
+        val col2: TextView = findViewById(R.id.col2)
+        val col3: TextView = findViewById(R.id.col3)
 
-
+        col1.text = HtmlCompat.fromHtml(getString(R.string.columna_1), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        col2.text = HtmlCompat.fromHtml(getString(R.string.columna_2), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        col3.text = HtmlCompat.fromHtml(getString(R.string.columna_3), HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
